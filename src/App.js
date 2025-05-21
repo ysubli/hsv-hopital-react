d585610e4fde17a0c9a187b4a8b1f7f0107d0e65
Sami <samichoqri@gmail.com>
2025-05-21 12:21:42 +0200

    button prendre rdv

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import Carousel from './components/Carousel';
 
 function App() {
   return (
@@ -9,6 +10,12 @@ function App() {
       <main className="main-content">
         <h1>Bienvenue à l'Hôpital HSV</h1>
         <p>Votre santé, notre priorité.</p>
+        <Carousel />
+        <div className="rdv-button-container">
+          <button className="rdv-button" onClick={() => window.location.href='#prise'}>
+            Prendre RDV
+          </button>
+        </div>
       </main>
     </div>
   );
